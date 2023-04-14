2da775bf9bfe74b37db059df8e02f370925644b8
Amazinggrace <grace.oduye@gmail.com>
2023-04-14 19:59:54 +0100

    test

--- __test__/test.ts
+++ __test__/test.ts
@@ -1,12 +1,17 @@
 import { ApolloServer } from "@apollo/server";
+import { access } from "fs";
 import { schema } from "../src/graphql/schema";
 
 test("returns account name input by user", async () => {
   const query = `#graphql
-    query getAccountName(bank_code: String, account_number:String, id:Sting) {
-        getAccountName(bank_code:$bank_code,account_number:$account_number,id:$id)
-      }
-  
+    query Query($getAccountNameId: String!, $accountNumber: String!, $bankCode: String!, $bankName: String) {
+          getAccountName(id: $getAccountNameId, account_number: $accountNumber, bank_code: $bankCode, bank_name: $bankName) {
+            first_name
+            last_name
+            middle_name
+
+          }
+}
   `;
 
   const testServer = new ApolloServer({
@@ -15,9 +20,10 @@ test("returns account name input by user", async () => {
   const result = await testServer.executeOperation({
     query,
     variables: {
-      bank_code: "044",
-      account_number: "0055852601",
-      id: "7fd8d46c-7225-4aff-91f2-94ae9c24a419",
+      accountNumber: "0055852601",
+      bankCode: "044",
+      getAccountNameId: "7fd8d46c-7225-4aff-91f2-94ae9c24a419",
+      bankName: null,
     },
   });
   console.log({ result });
@@ -27,6 +33,36 @@ test("returns account name input by user", async () => {
   expect(result.body).toBeTruthy();
 });
 
+test(" if user did not input account verify user by call to paystack", async () => {
+  const query = `#graphql
+    query Query($getAccountNameId: String!, $accountNumber: String!, $bankCode: String!, $bankName: String) {
+          getAccountName(id: $getAccountNameId, account_number: $accountNumber, bank_code: $bankCode, bank_name: $bankName) {
+            first_name
+            last_name
+            middle_name
+
+          }
+}
+  `;
+
+  const testServer = new ApolloServer({
+    schema,
+  });
+  const result = await testServer.executeOperation({
+    query,
+    variables: {
+      accountNumber: "0055852601",
+      bankCode: "044",
+      getAccountNameId: "7fd8d46c-7225-4aff-91f2-94ae9c24a419",
+      bankName: "access-bank",
+    },
+  });
+  console.log({ result });
+  // console.log({ data: result.body.singleResult });
+
+  expect(result.body.kind === "single");
+  expect(result.body).toBeTruthy();
+});
 /**
  * 
  * const server = new ApolloServer(config);
