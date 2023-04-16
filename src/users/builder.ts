2c4e930530e3c453a4fb1a40bf97cb013d2322e9
Amazinggrace <grace.oduye@gmail.com>
2023-04-17 00:29:06 +0100

    add default nullity true on schema

--- src/users/builder.ts
+++ src/users/builder.ts
@@ -6,11 +6,13 @@ import type PrismaTypes from "@pothos/plugin-prisma/generated";
 import { prisma } from "../config/db";
 
 export const builder = new SchemaBuilder<{
+  DefaultFieldNullability: true;
   Scalars: {
     Date: { Input: Date; Output: Date };
   };
   PrismaTypes: PrismaTypes;
 }>({
+  defaultFieldNullability: true,
   plugins: [PrismaPlugin],
   prisma: {
     client: prisma,
